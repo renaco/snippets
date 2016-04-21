273a0febf99de05a125267808fdde247bdf666c2
Renaco <renanromero@gmail.com>
2016-04-21 01:11:57 -0500

    wip(sunat) add location url

--- sunat/nav.js
+++ sunat/nav.js
@@ -1,9 +1,8 @@
 // login
+window.location='https://e-menu.sunat.gob.pe/cl-ti-itmenu/MenuInternet.htm';
+
+//inyect
 
 $('#txtRuc').val('10987654321');
 $('#txtUsuario').val('TUUSUER');
 $('#txtContrasena').val('tupass');
-
-// Ir a factura
-
-// Crear factura
