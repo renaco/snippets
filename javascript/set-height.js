bacc4d2c3b9dfcec9d76297460ecd7badb287ae2
Renán Romero <renanromero@gmail.com>
2016-09-07 11:59:58 -0500

    fix(set-height) cahnge method for call to tag name

--- javascript/set-height.js
+++ javascript/set-height.js
@@ -1,10 +1,10 @@
 function setHeight(){
   var getH = window.innerHeight,
       getW = window.innerWidth,
-      getHeader = document.getElementsByClassName('mdl-layout__header-row')[0].offsetHeight,
-      getFooter = document.getElementsByClassName('mdl-mini-footer')[0].offsetHeight;
+      getHeader = document.getElementsByTagName('header').offsetHeight,
+      getFooter = document.getElementsByTagName('footer').offsetHeight;
   var giveHeight = (getH - getFooter);
-  document.getElementById('wrapper').style.minHeight = giveHeight + 'px';
+  document.getElementById('container').style.minHeight = giveHeight + 'px';
   console.log(giveHeight);
 }
 
